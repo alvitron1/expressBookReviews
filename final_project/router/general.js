b2925d7e86f9e8d3bfa76249f75bffb4ed640078
Alvin Shanmugam <cajs121@gmail.com>
2025-03-18 00:49:25 -0400

    Finished up to part 5

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -18,26 +18,30 @@ public_users.get('/',function (req, res) {
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  res.status(200).json(books[isbn]);
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const author = req.params.author;
+    let filtered_books = Object.values(books).filter(book => book.author === author);
+    // Send the filtered_users array as the response to the client
+    res.send(filtered_books);
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const title = req.params.title;
+    let filtered_books = Object.values(books).filter(book => book.title === title);
+    // Send the filtered_users array as the response to the client
+    res.send(filtered_books);
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  res.status(200).json(books[isbn].reviews);
 });
 
 module.exports.general = public_users;
