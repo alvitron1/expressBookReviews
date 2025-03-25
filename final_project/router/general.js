ac459222290683da7d41575b8f908a2cfda348c8
Alvin Shanmugam <cajs121@gmail.com>
2025-03-25 01:20:49 -0400

    Finished Project

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -4,6 +4,8 @@ const e = require('express');
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
+const axios = require('axios');
+
 
 
 public_users.post("/register", (req,res) => {
@@ -28,19 +30,19 @@ public_users.post("/register", (req,res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
+public_users.get('/', async function (req, res) {
   //Write your code here
   return res.send(books);
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
+public_users.get('/isbn/:isbn', async function (req, res) {
   const isbn = req.params.isbn;
   res.status(200).json(books[isbn]);
  });
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
+public_users.get('/author/:author', async function (req, res) {
     const author = req.params.author;
     let filtered_books = Object.values(books).filter(book => book.author === author);
     // Send the filtered_users array as the response to the client
@@ -48,7 +50,7 @@ public_users.get('/author/:author',function (req, res) {
 });
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
+public_users.get('/title/:title', async function (req, res) {
     const title = req.params.title;
     let filtered_books = Object.values(books).filter(book => book.title === title);
     // Send the filtered_users array as the response to the client
@@ -56,7 +58,7 @@ public_users.get('/title/:title',function (req, res) {
 });
 
 //  Get book review
-public_users.get('/review/:isbn',function (req, res) {
+public_users.get('/review/:isbn', async function (req, res) {
   const isbn = req.params.isbn;
   res.status(200).json(books[isbn].reviews);
 });
